1407a8c6fac28eb4f700191d10e58e916a200227
Steven Adema <stevenadema@gmail.com>
2023-06-13 23:47:08 -0400

    dataframe formatting, create sites.json

--- main.py
+++ main.py
@@ -16,16 +16,19 @@ HEADERS = {
         "Cache-Control": "max-age=0",
     }
 
-# url = 'https://reservations.ontarioparks.com/api/availability/map?mapId=-2147483553&bookingCategoryId=5&equipmentCategoryId=null&subEquipmentCategoryId=null&cartUid=e4a5d5ee-11a7-4fe8-8a15-bf1fdd628878&cartTransactionUid=4b3943ba-54f1-4372-9dc0-a24a1a5cdd61&bookingUid=45fb3b00-018e-4902-9458-01d0d1390382&startDate=2023-05-13&endDate=2023-05-26&getDailyAvailability=true&isReserving=true&filterData=%5B%5D&boatLength=null&boatDraft=null&boatWidth=null&partySize=1&numEquipment=1&seed=2023-05-11T02:36:07.683Z'
+# Open the file in read mode
+with open('./mapping/sites.json', 'r') as file:
+    # Load the JSON data from the file
+    sites_dic = json.load(file)
+
+# url = 'https://reservations.ontarioparks.com/api/availability/map?mapId=-2147483553&bookingCategoryId=4&equipmentCategoryId=null&subEquipmentCategoryId=null&cartUid=4314e624-f8e1-4c7a-87ff-2992378289a0&cartTransactionUid=4b573d4a-04be-4a59-8b7c-66dbcbc6132a&bookingUid=422092c5-5fa0-4e2f-8fdb-2f24eb949aba&startDate=2023-08-26&endDate=2023-09-08&getDailyAvailability=true&isReserving=true&filterData=%5B%5D&boatLength=null&boatDraft=null&boatWidth=null&partySize=1&numEquipment=1&seed=2023-06-14T02:51:41.385Z'
 # data = requests.get(url, headers=HEADERS)
 # print(data)
-# # data = json.load(data)
+# exit()
+# data = json.load(data)
 # availabilities = data.json()['resourceAvailabilities']
 a = {'-2147479764': [{'availability': 1, 'remainingQuota': None}, {'availability': 1, 'remainingQuota': None}, {'availability': 1, 'remainingQuota': None}, {'availability': 0, 'remainingQuota': None}, {'availability': 1, 'remainingQuota': None}, {'availability': 0, 'remainingQuota': None}, {'availability': 1, 'remainingQuota': None}, {'availability': 1, 'remainingQuota': None}, {'availability': 1, 'remainingQuota': None}, {'availability': 0, 'remainingQuota': None}, {'availability': 0, 'remainingQuota': None}, {'availability': 0, 'remainingQuota': None}, {'availability': 0, 'remainingQuota': None}, {'availability': 1, 'remainingQuota': None}], '-2147479758': [{'availability': 1, 'remainingQuota': None}, {'availability': 1, 'remainingQuota': None}, {'availability': 1, 'remainingQuota': None}, {'availability': 1, 'remainingQuota': None}, {'availability': 0, 'remainingQuota': None}, {'availability': 0, 'remainingQuota': None}, {'availability': 1, 'remainingQuota': None}, {'availability': 1, 'remainingQuota': None}, {'availability': 1, 'remainingQuota': None}, {'availability': 0, 'remainingQuota': None}, {'availability': 0, 'remainingQuota': None}, {'availability': 0, 'remainingQuota': None}, {'availability': 1, 'remainingQuota': None}, {'availability': 1, 'remainingQuota': None}], '-2147479743': [{'availability': 1, 'remainingQuota': None}, {'availability': 1, 'remainingQuota': None}, {'availability': 1, 'remainingQuota': None}, {'availability': 0, 'remainingQuota': None}, {'availability': 0, 'remainingQuota': None}, {'availability': 1, 'remainingQuota': None}, {'availability': 1, 'remainingQuota': None}, {'availability': 1, 'remainingQuota': None}, {'availability': 1, 'remainingQuota': None}, {'availability': 0, 'remainingQuota': None}, {'availability': 0, 'remainingQuota': None}, {'availability': 0, 'remainingQuota': None}, {'availability': 0, 'remainingQuota': None}, {'availability': 1, 'remainingQuota': None}], '-2147479732': [{'availability': 1, 'remainingQuota': None}, {'availability': 1, 'remainingQuota': None}, {'availability': 0, 'remainingQuota': None}, {'availability': 0, 'remainingQuota': None}, {'availability': 0, 'remainingQuota': None}, {'availability': 0, 'remainingQuota': None}, {'availability': 1, 'remainingQuota': None}, {'availability': 1, 'remainingQuota': None}, {'availability': 1, 'remainingQuota': None}, {'availability': 0, 'remainingQuota': None}, {'availability': 0, 'remainingQuota': None}, {'availability': 0, 'remainingQuota': None}, {'availability': 0, 'remainingQuota': None}, {'availability': 1, 'remainingQuota': None}], '-2147479724': [{'availability': 1, 'remainingQuota': None}, {'availability': 1, 'remainingQuota': None}, {'availability': 1, 'remainingQuota': None}, {'availability': 1, 'remainingQuota': None}, {'availability': 1, 'remainingQuota': None}, {'availability': 1, 'remainingQuota': None}, {'availability': 1, 'remainingQuota': None}, {'availability': 1, 'remainingQuota': None}, {'availability': 1, 'remainingQuota': None}, {'availability': 1, 'remainingQuota': None}, {'availability': 1, 'remainingQuota': None}, {'availability': 1, 'remainingQuota': None}, {'availability': 1, 'remainingQuota': None}, {'availability': 1, 'remainingQuota': None}], '-2147479723': [{'availability': 1, 'remainingQuota': None}, {'availability': 1, 'remainingQuota': None}, {'availability': 1, 'remainingQuota': None}, {'availability': 1, 'remainingQuota': None}, {'availability': 0, 'remainingQuota': None}, {'availability': 0, 'remainingQuota': None}, {'availability': 1, 'remainingQuota': None}, {'availability': 1, 'remainingQuota': None}, {'availability': 1, 'remainingQuota': None}, {'availability': 0, 'remainingQuota': None}, {'availability': 0, 'remainingQuota': None}, {'availability': 1, 'remainingQuota': None}, {'availability': 1, 'remainingQuota': None}, {'availability': 1, 'remainingQuota': None}], '-2147479714': [{'availability': 1, 'remainingQuota': None}, {'availability': 1, 'remainingQuota': None}, {'availability': 1, 'remainingQuota': None}, {'availability': 1, 'remainingQuota': None}, {'availability': 1, 'remainingQuota': None}, {'availability': 1, 'remainingQuota': None}, {'availability': 1, 'remainingQuota': None}, {'availability': 1, 'remainingQuota': None}, {'availability': 1, 'remainingQuota': None}, {'availability': 1, 'remainingQuota': None}, {'availability': 1, 'remainingQuota': None}, {'availability': 1, 'remainingQuota': None}, {'availability': 1, 'remainingQuota': None}, {'availability': 1, 'remainingQuota': None}], '-2147479694': [{'availability': 1, 'remainingQuota': None}, {'availability': 0, 'remainingQuota': None}, {'availability': 1, 'remainingQuota': None}, {'availability': 1, 'remainingQuota': None}, {'availability': 1, 'remainingQuota': None}, {'availability': 0, 'remainingQuota': None}, {'availability': 1, 'remainingQuota': None}, {'availability': 1, 'remainingQuota': None}, {'availability': 1, 'remainingQuota': None}, {'availability': 0, 'remainingQuota': None}, {'availability': 1, 'remainingQuota': None}, {'availability': 1, 'remainingQuota': None}, {'availability': 0, 'remainingQuota': None}, {'availability': 1, 'remainingQuota': None}], '-2147479641': [{'availability': 1, 'remainingQuota': None}, {'availability': 0, 'remainingQuota': None}, {'availability': 1, 'remainingQuota': None}, {'availability': 1, 'remainingQuota': None}, {'availability': 0, 'remainingQuota': None}, {'availability': 0, 'remainingQuota': None}, {'availability': 1, 'remainingQuota': None}, {'availability': 1, 'remainingQuota': None}, {'availability': 1, 'remainingQuota': None}, {'availability': 0, 'remainingQuota': None}, {'availability': 0, 'remainingQuota': None}, {'availability': 0, 'remainingQuota': None}, {'availability': 0, 'remainingQuota': None}, {'availability': 1, 'remainingQuota': None}], '-2147479634': [{'availability': 1, 'remainingQuota': None}, {'availability': 0, 'remainingQuota': None}, {'availability': 0, 'remainingQuota': None}, {'availability': 1, 'remainingQuota': None}, {'availability': 0, 'remainingQuota': None}, {'availability': 0, 'remainingQuota': None}, {'availability': 1, 'remainingQuota': None}, {'availability': 1, 'remainingQuota': None}, {'availability': 1, 'remainingQuota': None}, {'availability': 0, 'remainingQuota': None}, {'availability': 0, 'remainingQuota': None}, {'availability': 0, 'remainingQuota': None}, {'availability': 0, 'remainingQuota': None}, {'availability': 1, 'remainingQuota': None}]}
 a = str(a)
-
-replace_list = [{"{'availability': 0, 'remainingQuota': None}":"NO"},
-                {"{'availability': 1, 'remainingQuota': None}":"YES"}]
 
 a = a.replace("{'availability': 1, 'remainingQuota': None}","1")
 a = a.replace("{'availability': 0, 'remainingQuota': None}","0")
@@ -33,24 +36,21 @@ a = a.replace("'",'"')
 
 a = json.loads(a)
 df = pd.DataFrame(a)
+df = df.transpose()
+
+# Create a replacement dictionary from siteId to siteName
+replace_dict = {site["siteId"]: site["siteName"] for site in sites_dic["sites"]}
+# Replace the index names
+df.rename(index=replace_dict, inplace=True)
+
+date_range = pd.date_range(start='08-23-2023', end='09-08-2023', periods=14).strftime('%b-%d')
+df.columns = date_range
+
 print(df)
-exit()
-print(availabilities)
-
-# create dataframe from json
-df = pd.DataFrame(availabilities)
-print(df)
 
 exit()
-
-
-
 payload = {}
 for param in request.split('?')[1].split('&'):
     key, value = param.split('=')
     payload[key] = value
 print(payload)
-
-
-
-exit()
