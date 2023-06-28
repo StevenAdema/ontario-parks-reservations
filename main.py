8c4cb5e800037073b2c2dd7c6bddc92cc70bf154
Steven Adema <stevenadema@gmail.com>
2023-06-27 23:22:05 -0400

    availability grid update, icon added

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QComboBox, QLabel, QCalendarWidget, QTableWidget, QTableWidgetItem, QPushButton, QTextEdit, QCheckBox, QLineEdit
-from PyQt5.QtCore import QDate
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QComboBox, QLabel, QCalendarWidget, QTableWidget, QTableWidgetItem, QPushButton, QTextEdit, QCheckBox, QLineEdit, QTableView
+from PyQt5.QtCore import QDate, QAbstractTableModel, Qt
+from PyQt5 import QtGui, QtWidgets
 from campsite import Campsite
+from pandasModel import pandasModel
 
 camp = Campsite("Charleston Lake")
-camp.get_park_availability_test('2023-08-23')
+camp_df = camp.get_park_availability_test('2023-08-23')
 
 class CampSiteApp(QWidget):
     def __init__(self):
@@ -17,15 +19,24 @@ class CampSiteApp(QWidget):
 
     def initUI(self):
         self.layout = QVBoxLayout()
+        self.setWindowTitle('Campsite Availability Checker')
+        self.setWindowIcon(QtGui.QIcon('./img/icon.png'))        
+            
         self.setStyleSheet(self.getStyleSheet(self.day_mode))
 
-        self.modeButton = QPushButton('Switch to Day Mode' if self.day_mode else 'Switch to Night Mode')
+        self.modeButton = QPushButton('Switch to Night Mode' if self.day_mode else 'Switch to Day Mode')
         self.modeButton.clicked.connect(self.switchMode)
         
         self.emailNotificationBox = QCheckBox('Email notifications')
         self.soundAlertBox = QCheckBox('Sound alert on new availability')
         self.emailInputBox = QLineEdit()
         self.emailInputBox.setPlaceholderText('Enter your email')
+
+        self.topLayout = QtWidgets.QHBoxLayout()
+        self.topLayout.addWidget(self.modeButton)
+        self.topLayout.addWidget(self.emailNotificationBox)
+        self.topLayout.addWidget(self.soundAlertBox)
+        self.topLayout.addWidget(self.emailInputBox)
 
         self.parkSelection = QComboBox()
         self.parkSelection.addItems(['Park 1', 'Park 2', 'Park 3', 'Park 4', 'Park 5'])
@@ -47,10 +58,8 @@ class CampSiteApp(QWidget):
         self.logContainer.setReadOnly(True)
         self.logContainer.append("Last refreshed at: N/A")
 
-        self.layout.addWidget(self.modeButton)
-        self.layout.addWidget(self.emailNotificationBox)
-        self.layout.addWidget(self.soundAlertBox)
-        self.layout.addWidget(self.emailInputBox)
+
+        self.layout.addLayout(self.topLayout)
         self.layout.addWidget(QLabel('Select a Park'))
         self.layout.addWidget(self.parkSelection)
         self.layout.addWidget(QLabel('Select a Date'))
@@ -69,12 +78,21 @@ class CampSiteApp(QWidget):
             if child.widget():
                 child.widget().deleteLater()
         self.initUI()
+    
+    def df_to_table(self, table, df):
+        table.setRowCount(df.shape[0])
+        table.setColumnCount(df.shape[1])
+        table.setHorizontalHeaderLabels(df.columns)
+        table.setVerticalHeaderLabels(df.index.map(str))
+        for i in range(df.shape[0]):
+            for j in range(df.shape[1]):
+                table.setItem(i, j, QtWidgets.QTableWidgetItem(str(df.iat[i, j])))
 
     def getStyleSheet(self, day_mode=True):
         if day_mode:
             return """
                 QWidget {
-                    background-color: #FBFBFB;
+                    background-color: #F0F0F0;
                     color: #383838;
                     font-family: 'Arial';
                 }
@@ -86,10 +104,20 @@ class CampSiteApp(QWidget):
                     font-size: 16px;
                     background-color: white;
                     border: 1px solid #B0B0B0;
+                    border-radius: 5px;    /* Rounded corners */
+                    padding: 10px;         /* Padding for comfort */
                 }
                 QCheckBox {
                     padding: 10px;
                     spacing: 20px;
+                }
+                QPushButton {
+                    background-color: #008CBA;  /* A light blue color */
+                    color: white;
+                    font-weight: bold;
+                }
+                QPushButton:hover {
+                    background-color: #007B9A;  /* A darker blue on hover */
                 }
                 QCheckBox::indicator {
                     width: 13px;
@@ -117,10 +145,20 @@ class CampSiteApp(QWidget):
                     font-size: 16px;
                     background-color: #3C4048;
                     border: 1px solid #282C34;
+                    border-radius: 5px;    /* Rounded corners */
+                    padding: 10px;         /* Padding for comfort */
                 }
                 QCheckBox {
                     padding: 10px;
                     spacing: 20px;
+                }
+                QPushButton {
+                    background-color: #3C8CBA;  /* A light blue color */
+                    color: white;
+                    font-weight: bold;
+                }
+                QPushButton:hover {
+                    background-color: #3C7B9A;  /* A darker blue on hover */
                 }
                 QCheckBox::indicator {
                     width: 13px;
@@ -133,6 +171,7 @@ class CampSiteApp(QWidget):
                     image: url(/path/to/your/checked-box-icon-dark.png);
                 }
             """
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
